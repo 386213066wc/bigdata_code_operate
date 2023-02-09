02948ae357533b648aca43c65abd897b33631154
386213066@qq.com <386213066@qq.com>
2023-02-09 10:07:13 +0800

    提交java代码

--- spark_code/src/main/scala/cn/spark/compute/VisitTopN.scala
+++ spark_code/src/main/scala/cn/spark/compute/VisitTopN.scala
@@ -2,14 +2,25 @@ package cn.spark.compute
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
 
 //对每个url计数；然后按次数降序排序；再取头5个
 object VisitTopN {
+  val  a:String = "20"
+
   def main(args: Array[String]): Unit = {
+
     //1、创建SparkConf对象
     val sparkConf: SparkConf = new SparkConf().setAppName("VisitTopN").setMaster("local[2]")
     //2、创建SparkContext对象
     val sc = new SparkContext(sparkConf)
+
+
+    //对spark程序某一时刻做快照
+    sc.setCheckpointDir("hdfs://bigdata01:8020/spark1/checkpoint")
+
+
+
     sc.setLogLevel("warn")
 
     //3、读取数据文件
@@ -18,11 +29,27 @@ object VisitTopN {
     //4、先对数据进行过滤
     val filterRDD: RDD[String] = dataRDD.filter(x => x.split(" ").length > 10)
 
+    filterRDD.checkpoint()
     //5、获取每一个条数据中的url地址链接
     val urlsRDD: RDD[String] = filterRDD.map(x => x.split(" ")(10))
 
+    filterRDD.flatMap(x => x.split(" "))
+
     //6、过滤掉不是http的请求
     val fUrlRDD: RDD[String] = urlsRDD.filter(_.contains("http"))
+
+    //对rdd数据进行缓存操作  将rdd数据给存放到内存当中去进行缓存
+    fUrlRDD.cache()  //就是调用了 persist()
+
+    //通过storageLevel来指定数据的存储级别  指定堆外内存  StorageLevel.OFF_HEAP  系统内存
+   // fUrlRDD.persist(StorageLevel.OFF_HEAP)
+
+    //如果缓存的数据丢失了怎么办？？？
+    fUrlRDD.persist(StorageLevel.MEMORY_AND_DISK_SER_2)
+
+    //取消RDD的数据缓存
+  //  fUrlRDD.unpersist(true)
+
 
     //7、把每一个url计为1
     val urlAndOneRDD: RDD[(String, Int)] = fUrlRDD.map(x => (x, 1))
